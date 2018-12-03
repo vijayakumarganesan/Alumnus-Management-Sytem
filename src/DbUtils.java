fdbef82d694fe8d17cec948f67d4173c47ae578a
vijayakumarganesan <vijayakumar10996@gmail.com>
2018-12-03 14:25:37 +0530

    Alumni students management system

---
+++ src/DbUtils.java
@@ -0,0 +1,21 @@
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+class DbUtils {
+
+    static TableModel resultSetToTableModule(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/mca_alumnus/MCA_Alumnus.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mca_alumnus;
+
+/**
+ *
+ * @author USER
+ */
+public class MCA_Alumnus {
+
+   
+  
+    public class loginform{
+    }
+    public class dbconnect{
+    
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MCA_Alumnus ma=new MCA_Alumnus();
+        
+                
+    }
+    
+}
